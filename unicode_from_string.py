6a55d414c0778e89a629fc85687f70597377f467
MichelML <michmoreau.l@gmail.com>
2019-02-26 13:48:26 -0500

    unicode from string v2

--- unicode_from_string.py
+++ unicode_from_string.py
@@ -1,7 +1,13 @@
+# version 1
 symbols = '$@#$%'
 codes = []
 
 for symbol in symbols:
   codes.append(ord(symbol))
 
+print(codes)
+
+# version 2
+symbols = '$@#$%'
+codes = [ord(symbol) for symbol in symbols]
 print(codes)
