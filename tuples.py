e114bc453240277e24cad00f07b5a32676c22c7f
MichelML <michmoreau.l@gmail.com>
2019-02-26 14:26:49 -0500

    lint

--- tuples.py
+++ tuples.py
@@ -1,19 +1,21 @@
-# Some introductory texts about Python present tuples as “immutable lists,” 
-# but that is short selling them. Tuples do double duty: they can be used as 
-# immutable lists and also as records with no field names. 
+# Some introductory texts about Python present tuples as “immutable lists,”
+# but that is short selling them. Tuples do double duty: they can be used as
+# immutable lists and also as records with no field names.
 # This use is sometimes overlooked, so we will start with that.
 
 # tuples as records
-# Tuples hold records: each item in the tuple holds the data for one field and the 
-# position of the item gives its meaning. 
+# Tuples hold records: each item in the tuple holds the data for one field and the
+# position of the item gives its meaning.
+
+import os
 
 lax_coordinates = (33.9425, -118.408056)
 traveler_ids = [('USA', '31195855'), ('BRA', 'CE34876'), ('ESP', 'XDA9347')]
 for passport in sorted(traveler_ids):
-  print('%s/%s' % passport)
+    print('%s/%s' % passport)
 
 for country, _ in traveler_ids:
-  print(country)
+    print(country)
 
 
 # Tuple unpacking
@@ -24,7 +26,7 @@ a = 1
 b = 2
 b, a = a, b
 c, d = 3, 4
-print(a,b,c,d)
+print(a, b, c, d)
 
 ## prefixing an argument with a star when calling a function
 divmod(20, 8)
