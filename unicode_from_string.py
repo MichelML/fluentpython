ee545d3885fae7d9a08221dd3d4796f9ce77acf8
MichelML <michmoreau.l@gmail.com>
2019-02-26 13:56:59 -0500

    string to unicode with filter map

--- unicode_from_string.py
+++ unicode_from_string.py
@@ -7,7 +7,14 @@ for symbol in symbols:
 
 print(codes)
 
-# version 2
+# version 2 (list comprehensions, or listcomps) - best solution
 symbols = '$@#$%'
 codes = [ord(symbol) for symbol in symbols]
 print(codes)
+
+# version 3 (map and filter)
+symbols = '$@#$%'
+beyond_ascii = [ord(s) for s in symbols if ord(s) > 35]
+print(beyond_ascii)
+beyond_ascii = list(filter(lambda c:c > 35, map(ord, symbols)))
+print(beyond_ascii)
