ed63ee127d66d4072d57e1e56f1c605b4b333f3d
MichelML <michmoreau.l@gmail.com>
2019-02-26 14:24:22 -0500

    more examples of tuple unpacking

--- tuples.py
+++ tuples.py
@@ -8,10 +8,27 @@
 # position of the item gives its meaning. 
 
 lax_coordinates = (33.9425, -118.408056)
-city, year, pop, chg, area = ('Tokyo', 2003, 32450, 0.66, 8014)
 traveler_ids = [('USA', '31195855'), ('BRA', 'CE34876'), ('ESP', 'XDA9347')]
 for passport in sorted(traveler_ids):
   print('%s/%s' % passport)
 
 for country, _ in traveler_ids:
   print(country)
+
+
+# Tuple unpacking
+city, year, pop, chg, area = ('Tokyo', 2003, 32450, 0.66, 8014)
+
+## swapping value of two variables
+a = 1
+b = 2
+b, a = a, b
+c, d = 3, 4
+print(a,b,c,d)
+
+## prefixing an argument with a star when calling a function
+divmod(20, 8)
+t = (20, 8)
+print(divmod(*t))
+quotient, remainder = divmod(*t)
+print(quotient, remainder)
