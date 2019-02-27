b872e8e64302510df0710b190a0083b5c5c9f810
MichelML <michmoreau.l@gmail.com>
2019-02-27 11:15:36 -0500

    add comments and start convention file

---
+++ python_conventions.md
@@ -0,0 +1,1 @@
+1. Functions or methods that change an object in place should return None to make it clear to the caller that the object itself was changed, and no new object was created.   
--- sequence_augmented_assigments.py
+++ sequence_augmented_assigments.py
@@ -12,3 +12,8 @@ print(id(t))
 t *= 2
 print(t)
 print(id(t))
+
+# recommendations from the author
+# 1. Putting mutable items in tuples is not a good idea.
+# 2. Augmented assignment is not an atomic operation we just saw it throwing an exception after doing part of its job.
+# 3. Inspecting Python bytecode is not too difficult, and is often helpful to see what is going on under the hood.
