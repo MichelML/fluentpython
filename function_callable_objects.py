45d38c34ee23bf2f6141859ec1f167d4eff6b70c
MichelML <michmoreau.l@gmail.com>
2019-03-02 12:25:36 -0500

    note different kind of callable objects

---
+++ function_callable_objects.py
@@ -0,0 +1,8 @@
+# 7 flavor of callable objects
+# 1. user-defined functions
+# 2. built-in functions
+# 3. built-in methods
+# 4. methods
+# 5. classes
+# 6. class instances
+# 7. generator functions
