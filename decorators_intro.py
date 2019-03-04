09e145fc83432aec9ec9c42b323412fbf2693d16
MichelML <michmoreau.l@gmail.com>
2019-03-04 12:25:39 -0500

    simple example of decorator

---
+++ decorators_intro.py
@@ -0,0 +1,14 @@
+# A decorator is a callable that takes another function as argument
+
+def decorate(callback):
+  return callback()
+
+@decorate
+def target():
+  print('running target()')
+
+# has the same effect as 
+# def target():
+#   print('running target()')
+
+# target = decorate(target)
