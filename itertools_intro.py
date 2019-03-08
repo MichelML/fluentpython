b0f62a0de3ceda779270f0c577db33e50d6bb627
MichelML <michmoreau.l@gmail.com>
2019-03-08 17:49:03 -0500

    itertools arithmetic

--- itertools_intro.py
+++ itertools_intro.py
@@ -3,3 +3,14 @@ gen = itertools.count(1, .5)
 
 for i in range(1,10):
   print(next(gen))
+
+def aritprog_gen(begin, step, end=None):
+  first = type(begin + step)(begin)
+  ap_gen = itertools.count(first, step)
+  if end is not None:
+    ap_gen = itertools.takewhile(lambda n: n < end, ap_gen)
+  return ap_gen
+
+gen2 = aritprog_gen(0, .2)
+for i in range(1,100):
+  print(next(gen2))
