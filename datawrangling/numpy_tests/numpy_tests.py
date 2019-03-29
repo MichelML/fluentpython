44b88a67b663ae2120fc70f8d5b12c8d017f0586
MichelML <michmoreau.l@gmail.com>
2019-03-29 14:54:28 -0400

    commit progress

--- datawrangling/numpy_tests/numpy_tests.py
+++ datawrangling/numpy_tests/numpy_tests.py
@@ -20,6 +20,7 @@ print(data.dtype)
 data1 = [6, 7.5, 8, 0, 1]
 arr1 = np.array(data1)
 print(arr1)
+print(arr1.dtype)
 
 # multidimensional ndarray
 data2 = [[1, 2, 3, 4], [5, 6, 7, 8]]
@@ -27,4 +28,4 @@ arr2 = np.array(data2)
 print(arr2)
 print(arr2.ndim)
 print(arr2.shape)
-
+print(arr2.dtype)
