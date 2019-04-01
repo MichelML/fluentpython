ce19ecb42b0ff04e96edbad0f7b9051f67d9f093
MichelML <michmoreau.l@gmail.com>
2019-04-01 14:41:03 -0400

    add array declaration methods

---
+++ datawrangling/numpy_tests/np_arrays.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+print(np.zeros(10))
+print(np.zeros((3,6)))
+print(np.zeros((3,6, 2)))
+print(np.empty((2, 3, 2)))
+print(np.arange(15))
+print(np.ones((10, 2)))
+print(np.full((2,2), 5))
+print(np.eye(5))
+print(np.identity(5))
